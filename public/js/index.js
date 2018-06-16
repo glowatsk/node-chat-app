a2fbfde17e6ca78455900191e76c1484316c36a3
glowatsk <glowatsk@gmail.com>
2018-06-16 15:33:08 -0600

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,13 +33,15 @@ socket.emit('createMessage', {
     console.log('Got it', data);
 });
 
+const messageTextbox = jQuery('[name=message]');
+
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -50,12 +52,17 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.')
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
+
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }); 
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.')
     });
 });
