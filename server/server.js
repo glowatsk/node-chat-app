0ec846d9456e55993ea9da2157b8df634a7a7569
glowatsk <glowatsk@gmail.com>
2018-06-16 09:17:37 +0000

    Added basic sockets to project

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path')
+const http = require('http');
 const express = require('express')
-const publicPath = path.join(__dirname, '/../public');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    
+    socket.on('disconnect', () => {
+        console.log('User was disconnect');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
 
