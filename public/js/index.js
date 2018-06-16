cc3de506c49a31aa611112440a034983440691ab
glowatsk <glowatsk@gmail.com>
2018-06-16 10:42:54 +0000

    Added emit newMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
         
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'test@example.com',
-        text: 'Hey hey hey'
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,15 +15,14 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-
-    socket.emit('newMessage', {
-        from: 'Brandon',
-        text: 'Hello World!',
-        createdAt: 1234
-    });
     
     socket.on('createMessage', (message) => {
         console.log('Create Message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
