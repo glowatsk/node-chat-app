465d53a99d96b838a17a873c86f2d41e6222d1bd
glowatsk <glowatsk@gmail.com>
2018-06-16 10:34:47 +0000

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,7 +14,18 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Brandon',
+        text: 'Hello World!',
+        createdAt: 1234
+    });
     
+    socket.on('createMessage', (message) => {
+        console.log('Create Message', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnect');
     });
